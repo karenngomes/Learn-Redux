c02c63769e897da5e3a619cc4ca066eee43a0a32
Karen Gomes <karen.nayara@audora.com.br>
2019-01-22 08:20:08 -0300

    redux reducer composition

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -7,7 +7,16 @@ const Comments = React.createClass({
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className="remove-comment">&times;</button>
+          <button
+            className="remove-comment"
+            onClick={this.props.removeComment.bind(
+              null,
+              this.props.params.postId,
+              i
+            )}
+          >
+            &times;
+          </button>
         </p>
       </div>
     );
@@ -17,8 +26,9 @@ const Comments = React.createClass({
     const { postId } = this.props.params;
     const author = this.refs.author.value;
     const comment = this.refs.comment.value;
-    
+
     this.props.addComment(postId, author, comment);
+    this.refs.commentForm.reset();
   },
 
   render() {
